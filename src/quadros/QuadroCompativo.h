e98a43b3949b736be48d97a3512162fb5660a385
JG <joaogabrielrsousa07@gmail.com>
2025-11-26 11:16:35 -0300

    Primeiro commit

---
+++ src/quadros/QuadroCompativo.h
@@ -0,0 +1,12 @@
+#include "QuadroResposta.h"
+#include "iteracao.h"
+
+class QuadroCompartivo {
+    public:
+        //ToDo: construtor (falar com o italo)
+        QuadroCompartivo();
+
+    private:
+        QuadroResposta QRPadrao;
+        QuadroResposta QRComFL;
+};
---
+++ src/quadros/QuadroResposta.h
@@ -0,0 +1,27 @@
+
+#include <string>
+#include <map>
+#include <vector> 
+#include "iteracao.h"
+
+enum NomeMetodo {
+    NEWTON_PADRAO_DER_CALC,
+    NEWTON_COM_FL_DER_CALC,
+    NEWTON_PADRAO_DER_NCALC,
+    NEWTON_COM_FL_DER_NCALC
+};
+
+class QuadroResposta{
+    public:
+        QuadroResposta(NomeMetodo metodo);
+        
+        void iterar_manual();
+        void iterar_total();
+
+        iteracao getIteracao(int indice);
+        double getTempo();
+    private:
+        std::vector <iteracao> quadro;
+        // Metodo metodo;
+        double tempo;
+};
