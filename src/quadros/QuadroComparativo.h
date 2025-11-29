d05385a65201401c9262ffadd2db07d7458422dd
JGabrielRS <joaogabrielrsousa07@gmail.com>
2025-11-29 16:11:22 -0300

    quadro comparativo começando a andar

---
+++ src/quadros/QuadroComparativo.h
@@ -0,0 +1,20 @@
+#include "QuadroResposta.h"
+#include "../iteracao/iteracao.h"
+
+class QuadroComparativo {
+    public:
+        QuadroComparativo(NomeMetodo metodo1, NomeMetodo metodo2, Polinomio &p, double x0, double epsilon, int n, std::vector<double> lambda);
+
+        void executarcomparacao();
+        void preencherHistorico();
+    private:
+        QuadroResposta quadro1;
+        QuadroResposta quadro2;
+        std::vector<iteracao> historico_newton_padrao;
+        std::vector<iteracao> historico_newton_fl;
+        Polinomio polinomio;
+        double x0;
+        double epsilon;
+        int n;
+        std::vector<double> lambda;
+};
